0cd199a3798e60ddb9f207269b39bd1ec0b3287d
Deniz TANIR <tanirdeniz35@gmail.com>
2025-03-03 17:27:20 +0300

    ilk R code

---
+++ ilkRcode.R
@@ -0,0 +1,16 @@
+print(log2(32))
+# plot(sin(seq(0,2*pi,length=100)))
+y=5
+x<-c(1,2,3,4,5)
+z=matrix(0,5,10)
+a=array(1:60,dim=c(3,4,5))
+print(a)
+denemeList1=list(isim="mustafa",yas=31,evliMi=F)
+denemeList2=list(isim="mustafa",yas=31,evliMi=F)
+denemeFrame=data.frame(isim=c("mustafa","ali")
+,yas=c(15,26),
+evliMi=c(F,T)
+)
+
+print(denemeFrame)
+print(denemeFrame[,1:2])
